88b06979308e18d09f022568ce22f70720570b87
LEEDAESEUNG-Developer <eotmd1016@gmail.com>
2022-12-09 20:17:02 +0900

    <프로젝트 완성, 도커 설정 완료> 휴... 드디어 해냈다

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,7 +1,9 @@
-insert into member(id, pwd) values
-                                (
-                                 'aaaa',
-                                 '1234'
-                                );
+insert into member(id, pwd)
+values ('aaaa',
+        '1234');
+insert into member(id, pwd)
+values ('docker',
+        '1234');
 
-select  * from member;
+select *
+from member;
--- sql/table-create.sql
+++ sql/table-create.sql
@@ -1,8 +1,10 @@
+create database dockerLogin;
+use dockerLogin;
+
 create table member(
     memberId int not null primary key auto_increment comment '회원번호',
     id varchar(40) not null comment '회원 아이디',
     pwd varchar(10) not null comment '회원 비밀번호'
 );
 
-use dockerLogin;
 select * from member;
